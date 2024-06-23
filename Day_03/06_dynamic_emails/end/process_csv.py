efe499761e12ed3bef373a175e4ca9f70ba6754a
Darshan Poudel <pranishpoudel10@gmail.com>
2024-06-23 08:07:33 +0545

    Day-3 removed from main

--- Day_03/06_dynamic_emails/end/process_csv.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Read the CSV file
-df = pd.read_csv("./sample.csv")
-
-# Iterate through the rows of the DataFrame
-for index, row in df.iterrows():
-    print(row["Name"], row["Email"])
